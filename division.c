4c1a0676e0cc5e29cbbe64243fd5d40576774d05
rimmi243 <rimmiamrutia5127@gmail.com>
2025-08-28 14:38:21 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a, b, divide;
+printf("enter two numbers");
+scanf("%d %d", &a,&b);
+divide=a/b;
+printf("divide = %d\n", divide);
+return0;
+}
---
+++ multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+ {
+    int a,b, multiply;
+    printf("enetr two numbers");
+    scanf("%d %d", &a, &b);
+    multiply = a*b;
+    printf("multiply= %d\n",multiply);
+    return0;
+}
